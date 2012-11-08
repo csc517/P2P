d714f7121e4728b04a78aa4d3736d464188c84fe
Chandan <choode@ncsu.edu>
2012-11-08 08:03:17 -0500

    half completed add files at startup add status

---
+++ src/Status.java
@@ -0,0 +1,28 @@
+
+public class Status {
+	int statusCode;
+	String message;
+		
+	public Status(int statusCode, String message) {
+		this.statusCode = statusCode;
+		this.message = message;
+	}
+	
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+}
